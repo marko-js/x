a9b4938f0318d4ed6189e20fedb4ddedb56a4106
Dylan Piercey <dpiercey@ebay.com>
2018-09-04 12:45:19 -0700

    fix: typo in static tag

--- packages/babel-plugin-htmljs/src/taglib/core/transform-static.js
+++ packages/babel-plugin-htmljs/src/taglib/core/transform-static.js
@@ -10,7 +10,7 @@ export default function(path) {
   const code = rawValue.replace(/^static\s*/, "").trim();
   const start = startTag.start + (rawValue.length - code.length);
   let { body } = hub.parse(code, start);
-  if ((body.length === 1) & t.isBlockStatement(body[0])) {
+  if (body.length === 1 && t.isBlockStatement(body[0])) {
     body = body[0].body;
   }
 
