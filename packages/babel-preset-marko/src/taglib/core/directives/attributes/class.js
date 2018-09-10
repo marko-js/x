15fa93c1fa6e33dcc24395eaf6613df89cbbe8d8
Dylan Piercey <dpiercey@ebay.com>
2018-09-09 18:53:24 -0700

    feat: add class helper

---
+++ packages/babel-preset-marko/src/taglib/core/directives/attributes/class.js
@@ -0,0 +1,21 @@
+import * as t from "../../../../definitions";
+export default function(path, attr) {
+  const { hub } = path;
+  const {
+    node: { value }
+  } = attr;
+  if (t.isStringLiteral(value)) return;
+  attr
+    .get("value")
+    .replaceWith(
+      t.callExpression(
+        hub.importNamed(
+          path,
+          "marko/src/runtime/helpers",
+          "cs",
+          "marko_class_merge"
+        ),
+        [value]
+      )
+    );
+}
