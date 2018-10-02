36b9deee94e4cb414ceab17ef6ed36cb087e8e9b
Dylan Piercey <dpiercey@ebay.com>
2018-10-01 22:30:43 -0700

    feat: add doctype tag

--- packages/babel-preset-marko/src/plugins/translate/index.js
+++ packages/babel-preset-marko/src/plugins/translate/index.js
@@ -1,3 +1,4 @@
+import HTMLDocumentType from "./html-document-type";
 import HTMLCDATA from "./html-cdata";
 import HTMLElement from "./html-element";
 import HTMLText from "./html-text";
@@ -5,6 +6,7 @@ import HTMLPlaceholder from "./html-placeholder";
 import HTMLScriptlet from "./html-scriptlet";
 
 export const visitor = {
+  HTMLDocumentType,
   HTMLCDATA,
   HTMLElement,
   HTMLText,
