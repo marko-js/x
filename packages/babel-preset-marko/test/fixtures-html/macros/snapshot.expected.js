9c8f2746a608bb69239286df141641d4e79a13c5
Dylan Piercey <dpiercey@ebay.com>
2018-10-01 22:00:56 -0700

    fix: enable more tests

---
+++ packages/babel-preset-marko/test/fixtures-html/macros/snapshot.expected.js
@@ -0,0 +1,40 @@
+import { t as _t } from "marko/src/html";
+import { r as _marko_renderer, c as _marko_defineComponent } from "marko/src/components/helpers";
+import { x as _marko_escapeXml } from "marko/src/runtime/html/helpers";
+
+function _renderTree(node, out) {
+  out.w(`Name:${_marko_escapeXml(node.name)}Children:`);
+
+  if (node.children) {
+    out.w("<ul>");
+
+    for (const child of node.children) {
+      out.w("<li>");
+
+      _renderTree({ ...child
+      }, out);
+
+      out.w("</li>");
+    }
+
+    out.w("</ul>");
+  }
+}
+
+const _marko_template = _t(__filename),
+      _marko_componentType = "/babel-preset-marko$1.0.0/test/fixtures-html/macros/template.marko";
+
+_marko_template._ = _marko_renderer(_marko_render, {
+  ___type: _marko_componentType
+})
+_marko_template.Component = _marko_defineComponent(null, _marko_template._)
+export default _marko_template;
+
+function _marko_render(input, out, __component, component, state) {
+  _renderTree({ ...input.node
+  }, out)
+}
+
+_marko_template.meta = {
+  id: "/babel-preset-marko$1.0.0/test/fixtures-html/macros/template.marko"
+}
