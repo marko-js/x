5306364e2e3f6338ee45954fc2098c9c24fed6ae
Dylan Piercey <dpiercey@ebay.com>
2018-10-13 21:12:24 -0700

    docs: add note for body only if

--- packages/babel-preset-marko/src/taglib/core/attributes/directives/body-only-if.js
+++ packages/babel-preset-marko/src/taglib/core/attributes/directives/body-only-if.js
@@ -1,4 +1,6 @@
 export default function(path, attr) {
+  // TODO should make this more generic.
+  // Needs to support more than just native elements.
   path.node.bodyOnlyIf = attr.get("value");
   attr.remove();
 }
