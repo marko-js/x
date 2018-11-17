5493b458135f6ba1425b6875392d895daf8e247d
Dylan Piercey <dpiercey@ebay.com>
2018-11-17 13:12:37 -0800

    test: add snapshot tests for code generation

--- packages/babel-plugin-marko/src/index.js
+++ packages/babel-plugin-marko/src/index.js
@@ -15,10 +15,17 @@ export default (api, options) => {
         isProduction
       });
       const ast = parse(hub);
-      const nodePath = hub.createNodePath();
-      nodePath.traverse(transform);
-      nodePath.traverse(translate);
-      nodePath.traverse(finalize);
+
+      // TODO: this package should be split into 3:
+      // 1. babel-syntax-marko (removes the need for the _parseOnly option)
+      // 2. babel-plugin-transform-marko (only runs transformers without converting Marko nodes to js)
+      // 3. babel-plugin-translate-marko (runs final translations)
+      if (!options._parseOnly) {
+        const nodePath = hub.createNodePath();
+        nodePath.traverse(transform);
+        nodePath.traverse(translate);
+        nodePath.traverse(finalize);
+      }
       return ast;
     }
   };
