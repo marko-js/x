769df571cad9806dd9ee135d03eb38d9cc1379d0
Dylan Piercey <dpiercey@ebay.com>
2019-09-26 16:22:58 -0700

    chore: update some tests

--- packages/translator-fluurt/test/fixtures/root-migration/migrator.js
+++
@@ -1,9 +0,0 @@
-import { types as t } from "@marko/babel-types";
-
-export default () => ({
-  Identifier(path) {
-    if (path.node.name === "old") {
-      path.replaceWith(t.identifier("new"));
-    }
-  }
-});
--- packages/translator-fluurt/test/fixtures/root-transform/transformer.js
+++
@@ -1,9 +0,0 @@
-import { types as t } from "@marko/babel-types";
-
-export default () => ({
-  Identifier(path) {
-    if (path.node.name === "before") {
-      path.replaceWith(t.identifier("after"));
-    }
-  }
-});
