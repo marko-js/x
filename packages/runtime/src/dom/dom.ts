e29f6c65ca9134397c954e671506d60ef5833515
LuLaValva <lukelavalva@gmail.com>
2023-02-08 15:43:45 -0800

    fix: !{undefined} is now empty string instead of space

--- packages/runtime/src/dom/dom.ts
+++ packages/runtime/src/dom/dom.ts
@@ -74,13 +74,17 @@ export function attrs(
 const doc = document;
 const parser = /* @__PURE__ */ doc.createElement("template");
 
-export function html(scope: Scope, value: string, index: number) {
+export function html(
+  scope: Scope,
+  value: string | undefined,
+  index: number /* TODO: change this type and similar ones to `accessor` */
+) {
   const firstChild = scope[index] as Node & ChildNode;
   const lastChild = (scope[index + "-"] || firstChild) as Node & ChildNode;
   const parentNode = firstChild.parentNode!;
   const afterReference = lastChild.nextSibling;
 
-  parser.innerHTML = value || " ";
+  parser.innerHTML = value || "";
   const newContent = parser.content;
   write(scope, index, newContent.firstChild);
   write(scope, (index + "-") as any as number, newContent.lastChild);
