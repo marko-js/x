113165eb8a4ccd3f238e4e1348757518f0c13b21
LuLaValva <lukelavalva@gmail.com>
2023-02-08 16:19:20 -0800

    test: return-tag [rename from yield-tag]

--- packages/translator/src/__tests__/fixtures/return-tag/__snapshots__/html.expected/template.js
+++ packages/translator/src/__tests__/fixtures/return-tag/__snapshots__/html.expected/template.js
@@ -1,18 +1,16 @@
 import { SYMBOL_OWNER as _SYMBOL_OWNER, nextScopeId as _nextScopeId, writeHydrateScope as _writeHydrateScope, register as _register, markHydrateControlSingleNodeEnd as _markHydrateControlSingleNodeEnd, write as _write, createRenderer as _createRenderer } from "@marko/runtime-fluurt/src/html";
-const _renderer = _register(({
-  show
-}, _tagVar, _scope0_) => {
+const _renderer = _register((input, _tagVar, _scope0_) => {
   const _scope0_id = _nextScopeId();
   let _ifScopeId;
   const _scope1_ = {},
     _ifRenderer = () => {};
-  if (show) {
+  if (input.show) {
     const _scope1_id = _nextScopeId();
     const _return = 1;
     _writeHydrateScope(_scope1_id, {
       [_SYMBOL_OWNER]: _scope0_id
     }, _scope1_);
-    _register(_ifRenderer, "packages/translator/src/__tests__/fixtures/yield-tag/template.marko_1_renderer");
+    _register(_ifRenderer, "packages/translator/src/__tests__/fixtures/return-tag/template.marko_1_renderer");
     _ifScopeId = _scope1_id;
   } else {
     const _scope2_id = _nextScopeId();
@@ -20,7 +18,7 @@ const _renderer = _register(({
     _writeHydrateScope(_scope2_id, {
       [_SYMBOL_OWNER]: _scope0_id
     }, undefined);
-    _register(_ifRenderer, "packages/translator/src/__tests__/fixtures/yield-tag/template.marko_2_renderer");
+    _register(_ifRenderer, "packages/translator/src/__tests__/fixtures/return-tag/template.marko_2_renderer");
     _ifScopeId = _scope2_id;
   }
   _write(`${_markHydrateControlSingleNodeEnd(_scope0_id, "#text/0", _ifScopeId)}`);
@@ -28,6 +26,6 @@ const _renderer = _register(({
     "#text/0!": _scope1_,
     "#text/0(": _ifRenderer
   }, _scope0_);
-}, "packages/translator/src/__tests__/fixtures/yield-tag/template.marko");
+}, "packages/translator/src/__tests__/fixtures/return-tag/template.marko");
 export default _renderer;
 export const render = /* @__PURE__ */_createRenderer(_renderer);
